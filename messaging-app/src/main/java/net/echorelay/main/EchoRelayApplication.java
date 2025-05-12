2f06a8551c8388eb2d00c0fba1adc1f6f6b344ba
Rishabh Tiwari <85021528+rishabh-147@users.noreply.github.com>
2025-05-13 00:36:12 +0530

    Initial Commit

---
+++ messaging-app/src/main/java/net/echorelay/main/EchoRelayApplication.java
@@ -0,0 +1,13 @@
+package net.echorelay.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoRelayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoRelayApplication.class, args);
+	}
+
+}
